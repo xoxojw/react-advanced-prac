d97e91b6a24272ead544e08b14427b517cc3cfaf
Jiwon Park <xoxojw.dev@gmail.com>
2023-07-05 02:00:38 +0900

    잘못 올린 폴더 삭제(중복폴더)

--- redux-basic-todo/src/pages/Detail.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import DetailBox from "../redux/components/DetailBox";
-
-function Detail() {
-  return <DetailBox />;
-}
-
-export default Detail;
--- redux-basic-todo/src/redux/components/common/FormButton.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function FormButton() {
-  return <button>제출</button>;
-}
-
-export default FormButton;
